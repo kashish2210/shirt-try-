9510b0f9f1b32215c727dc810854b05f5244f2fe
kashish2210 <153910815+kashish2210@users.noreply.github.com>
2024-07-29 21:24:31 +0530

    Add files via upload

--- shirt try/main.py
+++ shirt try/main.py
@@ -3,15 +3,16 @@ import cvzone
 import cv2
 from cvzone.PoseModule import PoseDetector
 
-cap = cv2.VideoCapture("Resources/Videos/1.mp4")
+cap = cv2.VideoCapture(r"Resources\Videos\1.mp4")
 detector = PoseDetector()
 
-shirtFolderPath = "Resources/Shirts"
+shirtFolderPath = os.path.join("Resources", "Shirts")
+print(shirtFolderPath)
 listShirts = os.listdir(shirtFolderPath)
 fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
 shirtRatioHeightWidth = 581 / 440
 imageNumber = 0
-imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
+imgButtonRight = cv2.imread(r"Resources\button.png", cv2.IMREAD_UNCHANGED)
 imgButtonLeft = cv2.flip(imgButtonRight, 1)
 counterRight = 0
 counterLeft = 0
@@ -24,12 +25,13 @@ while True:
         break
 
     img = detector.findPose(img)
-    img = cv2.flip(img, 1)
+    # img = cv2.flip(img, 1)
     lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
     if lmList:
+
         center = bboxInfo["center"]
-        lm11 = lmList[11][1:3]
-        lm12 = lmList[12][1:3]
+        lm11 = lmList[11][0:2]
+        lm12 = lmList[12][0:2]
 
         # Debugging prints
         print(f"lm11: {lm11}, lm12: {lm12}")
@@ -41,13 +43,16 @@ while True:
             imgShirtPath = os.path.join(shirtFolderPath, listShirts[imageNumber])
             print(f"Loading shirt image: {imgShirtPath}")
             imgShirt = cv2.imread(imgShirtPath, cv2.IMREAD_UNCHANGED)
-            
+
             if imgShirt is not None:
                 print(f"Loaded shirt image with shape: {imgShirt.shape}")
-                imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+                imgShirt = cv2.resize(
+                    imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth))
+                )
 
                 currentScale = (lm11[0] - lm12[0]) / 190
                 offset = int(44 * currentScale), int(48 * currentScale)
+
                 overlay_coords = (lm12[0] - offset[0], lm12[1] - offset[1])
                 print(f"Overlay coordinates: {overlay_coords}")
 
@@ -61,18 +66,34 @@ while True:
         img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
         img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
 
-        if lmList[16][1] < 300:
+        if lmList[16][0] < 300:
             counterRight += 1
-            cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
-                        counterRight * selectionSpeed, (0, 255, 0), 20)
+            cv2.ellipse(
+                img,
+                (139, 360),
+                (66, 66),
+                0,
+                0,
+                counterRight * selectionSpeed,
+                (0, 255, 0),
+                20,
+            )
             if counterRight * selectionSpeed > 360:
                 counterRight = 0
                 if imageNumber < len(listShirts) - 1:
                     imageNumber += 1
-        elif lmList[15][1] > 900:
+        elif lmList[15][0] > 900:
             counterLeft += 1
-            cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
-                        counterLeft * selectionSpeed, (0, 255, 0), 20)
+            cv2.ellipse(
+                img,
+                (1138, 360),
+                (66, 66),
+                0,
+                0,
+                counterLeft * selectionSpeed,
+                (0, 255, 0),
+                20,
+            )
             if counterLeft * selectionSpeed > 360:
                 counterLeft = 0
                 if imageNumber > 0:
@@ -82,7 +103,7 @@ while True:
             counterLeft = 0
 
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
